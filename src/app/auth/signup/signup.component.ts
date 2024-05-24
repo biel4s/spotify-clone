2896eab299d29f082b775d476a28e12f9129cba9
biel4s <kamilbielawski13@gmail.com>
2024-05-24 14:05:08 +0200

    Added signup page

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,25 +1,38 @@
 import { Component } from '@angular/core';
 import {FooterComponent} from "../../shared/components/footer/footer.component";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
+import {NgIf} from "@angular/common";
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [
-    FooterComponent,
-    FormsModule,
-    MatFormField,
-    MatInput,
-    MatLabel,
-    ReactiveFormsModule,
-    MatButton
-  ],
+    imports: [
+        FooterComponent,
+        FormsModule,
+        MatFormField,
+        MatInput,
+        MatLabel,
+        ReactiveFormsModule,
+        MatButton,
+        NgIf
+    ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  readonly signupForm = this.fb.nonNullable.group({
+    account: ['', [Validators.required, Validators.email]],
+  })
 
+  constructor(private fb: FormBuilder, public readonly authService: AuthService, private router: Router) {
+  }
+
+  onSubmit(): void {
+
+  }
 }

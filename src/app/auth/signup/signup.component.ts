25568d9325b513405c2c8c2105003b699471b473
biel4s <kamilbielawski13@gmail.com>
2024-05-30 17:18:29 +0200

    Added error box and template if email is correct

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,38 +1,49 @@
 import { Component } from '@angular/core';
 import {FooterComponent} from "../../shared/components/footer/footer.component";
 import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {NgIf} from "@angular/common";
-import {AuthService} from "../auth.service";
+import {AuthService, Credentials} from "../auth.service";
 import {Router} from "@angular/router";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-    imports: [
-        FooterComponent,
-        FormsModule,
-        MatFormField,
-        MatInput,
-        MatLabel,
-        ReactiveFormsModule,
-        MatButton,
-        NgIf
-    ],
+  imports: [
+    FooterComponent,
+    FormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    MatButton,
+    NgIf,
+    MatError,
+    MatIcon
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  isEmailCorrect: boolean = false;
+
   readonly signupForm = this.fb.nonNullable.group({
-    account: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['']
   })
 
   constructor(private fb: FormBuilder, public readonly authService: AuthService, private router: Router) {
   }
 
-  onSubmit(): void {
+  changeState(): void {
+    this.isEmailCorrect = true;
+  }
 
+  onSubmit(): void {
+    const credentials: Credentials = this.signupForm.getRawValue();
+    this.authService.createWithEmail(credentials.email, credentials.password);
   }
 }
